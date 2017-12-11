6ad562c50563557df460312704957e66a0dc541c
Jorge <jorgeserranojunior@hotmail.com>
2017-12-10 23:21:00 -0200

    Primeiro Commit

---
+++ public/app.php
@@ -0,0 +1,12 @@
+<?php
+
+    $dados = [
+        "categorias" => [
+            ["nome" => "ESSENCIAIS", "contas" => "15", "cor" => "panel-info"],
+            ["nome" => "INVESTIMENTO", "contas" => "25", "cor" => "panel-success"],
+        ],
+    ];
+
+  
+
+?>
---
+++ public/js/index.js
@@ -0,0 +1,30 @@
+var dados = {
+    itens: 
+    {
+        categorias:{
+            essenciais: {
+                contas:{
+                    telefone: {
+                        valor: "150,00",
+                    },
+                    luz: {
+                        valor: "100,00",
+                    },
+                    Agua: {
+                        valor: "50,00",
+                    },
+                }
+            },
+            investimentos: {
+                MEI: {               
+                    valor: "55,00"
+                } 
+            }
+        }
+    }   
+};
+
+console.log(dados.itens.categorias);
+
+
+
