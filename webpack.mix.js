0ec59ee457eebb37d42da6a129422e25507cfdce
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2017-12-27 18:31:02 -0200

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ let mix = require('laravel-mix');
 
 // mix.js('resources/assets/js/app.js', 'public/js');
    
-mix.sass('resources/assets/sass/app.scss', 'public/css/all.css')
+mix.sass('resources/assets/sass/style.scss', 'public/css')
 
