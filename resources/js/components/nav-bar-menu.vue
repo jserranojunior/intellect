2af0ad05b0262436c33174048869705dd2558ef2
jserranojunior <jorgeserranojunior@hotmail.com>
2018-11-08 15:28:30 -0200

    identfier

--- resources/js/components/nav-bar-menu.vue
+++ resources/js/components/nav-bar-menu.vue
@@ -1,20 +1,18 @@
 <template>
-    <div>
-        <ul class="nav navbar-nav d-md-down-none">
-                    <li class="nav-item px-3">
-                        <a class="nav-link" href="/intellect">Home</a>
-                    </li>
-                    <li class="nav-item px-3">
-                        <a class="nav-link" href="/intellect/financeiro">Financeiro</a>
-                    </li>
-
-                </ul>
-    </div>
+<div>
+    <ul class="nav navbar-nav d-md-down-none">
+        <li class="nav-item px-3">
+            <a class="nav-link" href="/intellect">Home</a>
+        </li>
+        <li class="nav-item px-3">
+            <a class="nav-link" href="/intellect/financeiro">Financeiro</a>
+        </li>
+    </ul>
+</div>
 </template>
 
 <script>
 export default {
-    name:"nav-bar-menu",
-}
+    name: "nav-bar-menu"
+};
 </script>
-
