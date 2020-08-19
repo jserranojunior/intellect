cfe217a961c371030d67b640e5e065cf3891c811
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2020-08-19 19:33:34 -0300

    Docker and Quasar

---
+++ quasar/src/pages/Financial.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page>
+    <div class="flex">
+      <div class="card-tail  w-3/12 shadow m-2 p-2 flex">
+        <div class="header-tail w-full border-b mb-2 py-2 text-center">
+        Primordial
+      </div>
+      <div class="body-tail w-full">
+        Valores
+      </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+<style>
+@import "tailwindcss/base";
+
+@import "tailwindcss/components";
+
+@import "tailwindcss/utilities";
+</style>
---
+++ quasar/src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg" />
+    <p class="text-xl text-green-600">FUNCIONANDO</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+}
+</script>
+<style>
+@import "tailwindcss/base";
+
+@import "tailwindcss/components";
+
+@import "tailwindcss/utilities";
+</style>
