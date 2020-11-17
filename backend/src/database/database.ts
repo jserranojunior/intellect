8f18c2fa0574dd3ebe707748239ed570f7c0fb2f
Jorge S Junior <jorgeserranojunior@hotmail.com>
2020-11-17 20:19:12 -0300

    remove funcion connect

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,13 +1,5 @@
 import connection from "./knexfile";
 import knex from "knex";
 
-function connect(){
-  try{
-  return knex(connection)
-}catch(erro){
-  console.log(erro)
-  return erro
-}
-}
 
-export default connect();
+export default knex(connection);
