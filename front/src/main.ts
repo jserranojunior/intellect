1ca3c70074817fc8da4e53168f79346b60674bd4
Jorge S Junior <jorgeserranojunior@hotmail.com>
2021-04-04 15:44:55 -0300

    Refactory debbuger

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,10 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./assets/styles/tail.css";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-
-const oi = "ola";
-console.log(oi);
 
 import router from "./router/index";
 
